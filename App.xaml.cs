682336978bf2511e37ee2c5daef60884dbc0a2a7
Subhash Venkatesan <44364933+subhash199@users.noreply.github.com>
2025-10-01 23:29:02 +0100

    Sync services mostly and UI changes and printer rework

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Maui.Devices;
+﻿
+using EposRetail.Services;
+using Microsoft.Maui.Devices;
 namespace EposRetail
 {
     public partial class App : Application
     {
+
         public App()
         {
             InitializeComponent();
@@ -11,14 +14,14 @@ namespace EposRetail
         protected override Window CreateWindow(IActivationState? activationState)
         {
             var window = new Window(new MainPage()) { Title = "EposRetail" };
-            
+
             // Set window to fullscreen/maximized by using screen dimensions
             var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
             window.Width = displayInfo.Width / displayInfo.Density;
             window.Height = displayInfo.Height / displayInfo.Density;
             window.X = 0;
             window.Y = 0;
-            
+
             return window;
         }
     }
